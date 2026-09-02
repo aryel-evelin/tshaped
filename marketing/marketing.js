412bf7b8492958ad4f2abc0c2b68783d58d5bdd5
Aryel Evelin <63274585+aryel-evelin@users.noreply.github.com>
2026-09-02 19:47:49 -0300

    Update marketing.js

--- marketing/marketing.js
+++ marketing/marketing.js
@@ -13,11 +13,11 @@ function updateLanguage(lang) {
 
     const resumeLink = document.getElementById('resume-link');
 
-    if (lang === 'pt') {
-        if (resumeLink) resumeLink.href = '../curriculo_pt.pdf';
-    } else {
-        if (resumeLink) resumeLink.href = '../curriculo_en.pdf';
-    }
+   if (lang === 'pt') {
+    if (resumeLink) resumeLink.href = '/tshaped/curriculo_marketing_pt.pdf';
+} else {
+    if (resumeLink) resumeLink.href = '/tshaped/curriculo_marketing_en.pdf';
+}
 
     document.querySelectorAll('[data-pt]').forEach(el => {
         const text = el.getAttribute(`data-${lang}`);
